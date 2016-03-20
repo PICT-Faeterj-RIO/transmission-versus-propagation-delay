c915ed04184b937b00d26a4f2f9730cdbd5b7569
Beatriz Lima <biaaml@hotmail.com>
2016-03-20 14:12:22 -0300

    Adicionado Cenário 1

--- js/animacao.js
+++ js/animacao.js
@@ -3,27 +3,33 @@ var y = 116;
 var z = 25;
 var w = 500;
 var timer;
+var a = 200;
+var b = 530;
 
 function mover() {
     z+=1;
     context.clearRect(0, 0, canvas.width, canvas.height); //Limpa o canvas
     context.drawImage(imagem, 0, 0, canvas.width, canvas.height);
-    context.drawImage(imagem2, x, y, 96, 64); //Desenha de novo
+    if(situ==2){context.drawImage(imagem2, x, y, 96, 64);} //Desenha de novo
     context.drawImage(imagem3, z, 130, 40, 40); //Desenha de novo
     if(z>=100){
       x+=1;
       context.clearRect(0, 0, canvas.width, canvas.height); //Limpa o canvas
       context.drawImage(imagem, 0, 0, canvas.width, canvas.height);
-      context.drawImage(imagem2, x, y, 96, 64); //Desenha de novo
-      if(x>=canvas.width-200)
+      if(situ==1){a+=1; context.drawImage(imagem2, a, 130, 40, 40);} //Desenha de novo
+      if(situ==2){context.drawImage(imagem2, x, y, 96, 64);}
+      if(x>=canvas.width-200 || a>=canvas.width-220)
       {
         x=canvas.width-200;
         w+=1;
+        b+=1;
         context.clearRect(0, 0, canvas.width, canvas.height); //Limpa o canvas
         context.drawImage(imagem, 0, 0, canvas.width, canvas.height);
-        context.drawImage(imagem2, x, y, 96, 64); //Desenha de novo
-        context.drawImage(imagem3, w, 130, 40, 40); //Desenha de novo
-        if(w>=canvas.width-50){
+        if(situ==1){context.drawImage(imagem3, b, 130, 40, 40);}
+        if(situ==2){
+        context.drawImage(imagem2, x, y, 96, 64);
+        context.drawImage(imagem3, w, 130, 40, 40);} //Desenha de novo
+        if(w>=canvas.width-50 || b>=canvas.width-50){
           stop();
         }
       }
@@ -31,13 +37,16 @@ function mover() {
 }
 
 function resetar() {
+  stop();
   x=100;
   y=116;
   z=25;
   w=500;
+  a=200;
+  b=530;
   context.clearRect(0, 0, canvas.width, canvas.height); //Limpa o canvas
   context.drawImage(imagem, 0, 0, canvas.width, canvas.height);
-  context.drawImage(imagem2, x, y, 96, 64); //Desenha de novo
+  if(situ==2){context.drawImage(imagem2, x, y, 96, 64);}
   context.drawImage(imagem3, z, 130, 40, 40); //Desenha de novo
 }
 
